8e397144c68754599b8fd25170d36985093a5673
OsmarElProgramador <barrazita11@gmail.com>
2020-05-15 18:52:01 -0700

    hola

--- js/main.js
+++ js/main.js
@@ -1,46 +1,12 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
-
-const{app,BrowserWindow,Menu} = electron;
-
-let loginWindow;
-let mainWindow;
-
-app.on('ready', function() {
-    loginWindow = new BrowserWindow({});
-    loginWindow.maximize();
-    loginWindow.loadURL(url.format({
-        pathname: path.join('../login.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-
-    loginWindow.on('closed', () =>{
-        app.quit();
-    })
-
-    const menu = Menu.buildFromTemplate(mainMenu);
-    Menu.setApplicationMenu(menu);
-})
-
-function mainView(){
-    mainWindow = new BrowserWindow({});
-    mainWindow.maximize();
-    mainWindow.loadURL(url.format({
-        pathname: path.join('../index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+function back(){
+    window.location.replace('index.html');
 }
 
-const mainMenu = [
-    {
-        label: 'Ir a login',
-        click(){
-            loginWindow.hide();
-            mainView();
-        }
-    }
-]
+function sentEmail(){
+ var mail = "mailto:algomez@iswug.net?subject=Asesorias ITSON&body="
+ var subject = document.getElementById('text').value;
+ var res = mail.concat(subject);
+ window.open(res);
+ window.location.replace('index.html');
+}
 
