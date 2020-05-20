84b05ac8f5a93a10330d2463dbc9ca0a2ccbc20b
OsmarElProgramador <barrazita11@gmail.com>
2020-05-20 02:15:10 -0700

    Muchas cosas que no había subido por wey

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ function back(){
     window.location.replace('index.html');
 }
 
+function addAdvisoryToTable(){
+
+}
+
 function sentEmail(){
  var mail = "mailto:algomez@iswug.net?subject=Asesorias ITSON&body="
  var subject = document.getElementById('text').value;
@@ -9,4 +13,13 @@ function sentEmail(){
  window.open(res);
  window.location.replace('index.html');
 }
+    
+function goToAsesorias(){
+    document.getElementById('asesoria');
+    window.location.replace('coursesView.html');    
+}
 
+function goToAsesor(){
+    document.getElementById('asesor');
+    window.location.replace('formAsesor.html');
+}
